10bd39b4e8e18718bb6d4655e4d43e0814f51a2f
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-28 17:53:04 -0400

    Adding documentation comments

--- demo/src/utils.rs
+++ demo/src/utils.rs
@@ -4,12 +4,23 @@ use hex;
 use std::str;
 use num_bigint::BigUint;
 
+/// keccak value for Transfer(address,address,uint256)
 const TRANSFER_TOPIC: &str = "ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
+/// Encodes an array of bytes in hex
+///
+/// `input`: Bytes
+///
+/// returns `String`: hex encoded input
 pub fn address_pretty(input: &[u8]) -> String {
     format!("0x{}", hex::encode(input))
 }
 
+/// Checks if the given log is an erc20 transfer event by check the keccak value of the log topic
+///
+/// `log`: A log from a call
+///
+/// returns `bool`: true if erc20 transfer event, else false
 pub fn is_erc20transfer_event(log: &pb::eth::Log) -> bool {
     if log.topics.len() != 3 || log.data.len() != 32 {
         return false;
@@ -17,6 +28,12 @@ pub fn is_erc20transfer_event(log: &pb::eth::Log) -> bool {
     return hex::encode(&log.topics[0]) == TRANSFER_TOPIC;
 }
 
+/// Find the erc20 storage changes (new and old valance of given holder)
+///
+/// `call`: Call containing storage changes
+/// `holder`: Address of the holder (sender or the receiver)
+///
+/// returns `Vec<BalanceChange`: List of balance changes for a given holder
 pub fn find_erc20_storage_changes(call: &pb::eth::Call, holder: &Vec<u8>) -> Vec<pb::erc20::BalanceChange> {
     let mut out: Vec<pb::erc20::BalanceChange> = Vec::new();
     let keys = erc20storage_keys_from_address(call, holder);
@@ -42,6 +59,12 @@ pub fn find_erc20_storage_changes(call: &pb::eth::Call, holder: &Vec<u8>) -> Vec
     return out;
 }
 
+/// Get the storage keys from an address given a call
+///
+/// `call`: Call containing storage keys
+/// `addr`: Address of the call
+///
+/// returns `Vec<String>`: List of hashes of keccak preimages
 fn erc20storage_keys_from_address(call: &pb::eth::Call, addr: &Vec<u8>) -> Vec<String> {
     let mut out: Vec<String> = Vec::new();
     let addr_as_hex = hex::encode(addr);
