bbd8e471175f2ae845990f570582b39cbcac99ce
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2022-03-29 10:48:55 -0400

    clean up some useless comments

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -10,21 +10,16 @@ use substreams::{log, proto, state};
 /// `block_len`: Length of the block in wasm heap memory
 #[no_mangle]
 pub extern "C" fn map_hello_world(block_ptr: *mut u8, block_len: usize) {
-    // register panic hook to forward any panic to the executable
     substreams::register_panic_hook();
 
-    // decode the block in the heap by using decode_ptr method from substreams lib
     let blk: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
 
-    // loop over every transaction
     for trx in blk.transaction_traces {
-
-        // say hello
         log::println(format!("Hello, transaction sender: {}", utils::address_pretty(trx.from.as_slice())));
         log::println(format!("Hello, transaction receiver: {}", utils::address_pretty(trx.to.as_slice())));
 
-        substreams::output(trx); // output the transaction
-        break; // then get out
+        substreams::output(trx);
+        break;
     }
 }
 
@@ -44,23 +39,15 @@ pub extern "C" fn map_hello_world(block_ptr: *mut u8, block_len: usize) {
 /// `block_len`: Length of the block in wasm heap memory
 #[no_mangle]
 pub extern "C" fn map_erc_20_transfer(block_ptr: *mut u8, block_len: usize) {
-    // register panic hook to forward any panic to the executable
     substreams::register_panic_hook();
 
-    // decode the block in the heap by using decode_ptr method from substreams lib
     let block: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
 
-    // declare transfers which will contain all the erc20 transfers
     let mut transfers = pb::erc20::Transfers { transfers: vec![] };
 
-    // loop over every transaction
     for trx in block.transaction_traces {
-        // loop over every call for a given transaction
         for call in trx.calls {
-            // loop over every log in a given call
             for log in call.clone().logs {
-
-                // check if the log is an erc20 transfer event
                 if !utils::is_erc20transfer_event(&log) {
                     continue
                 }
@@ -71,7 +58,6 @@ pub extern "C" fn map_erc_20_transfer(block_ptr: *mut u8, block_len: usize) {
                 let amount = &log.data[0..32];
                 let log_ordinal = log.index as u64;
 
-                // create transfer event
                 let transfer_event = pb::erc20::Transfer {
                     from: utils::address_pretty(from_addr.as_slice()),
                     to: utils::address_pretty(to_addr.as_slice()),
@@ -81,29 +67,32 @@ pub extern "C" fn map_erc_20_transfer(block_ptr: *mut u8, block_len: usize) {
                     log_ordinal
                 };
 
-                // add the event to the transfers in the transfers' list
                 transfers.transfers.push(transfer_event);
             }
         }
     }
 
-    // output transfers
     substreams::output(transfers);
 }
 
 #[no_mangle]
 pub extern "C" fn build_erc_20_transfer_state(transfers_ptr: *mut u8, transfers_len: usize) {
-    // register panic hook to forward any panic to the executable
     substreams::register_panic_hook();
 
-    // decode the transfers in the heap by using decode_ptr method from substreams lib
     let transfers: pb::erc20::Transfers = proto::decode_ptr(transfers_ptr, transfers_len).unwrap();
 
-    // loop over every transfer
     for transfer in transfers.transfers {
-
-        // store the transfer in the store
-        state::set(transfer.log_ordinal as i64, format!("transfer:{},{}", transfer.from, transfer.to),proto::encode(&transfer).unwrap())
+        state::set(1, format!("transfer:{}:{}", transfer.from, transfer.to),proto::encode(&transfer).unwrap())
     }
 }
 
+#[no_mangle]
+pub extern "C" fn map_erc_20_transfer_per_hour(block_ptr: *mut u8, block_len: usize) {
+    substreams::register_panic_hook();
+
+    let block: pb::eth::Block = proto::decode_ptr(block_ptr, block_len).unwrap();
+
+    for trx in block.transaction_traces {
+
+    }
+}
